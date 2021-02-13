dd0f25e6f46fbeaee5aa84126e79322383f8b545
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2021-02-13 17:16:54 +0900

    機能追加

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Detail,Post
+from .models import Detail,Post,Category
 
 admin.site.register(Detail)
 admin.site.register(Post)
+admin.site.register(Category)
